ea94d30ac8f8cfc2e03c837797c05c47e418b3cb
Yog <yog.panjarale@gmail.com>
2021-11-24 18:39:58 +0530

    static files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 require("dotenv").config();
 import express from "express";
 import { connect } from "mongoose";
+import path from "path";
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/test";
 connect(MONGO_URI).then(() => console.log("MongoDB Connected..."));
 
 const app = express();
 
+app.use("/",express.static(path.resolve(__dirname,"..","public")));
+
 app.get("/", (_req, res) => {
 	res.send("Hello World!");
 });
-app.get("ping", (_req, res) => {
+app.get("/ping", (_req, res) => {
 	res.send("pong");
 });
 const PORT = process.env.PORT || 3000;
